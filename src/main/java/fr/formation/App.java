486494ba5b8e1880b1b37511c5ca00b5cdae2208
ChrisMag06 <Christophe.maggiore@libertysurf.fr>
2024-02-01 16:17:31 +0100

    first commit

---
+++ src/main/java/fr/formation/App.java
@@ -0,0 +1,16 @@
+package fr.formation;
+
+import fr.formation.services.Calcul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+            Calcul sommeCalcul = new Calcul();
+            sommeCalcul.somme(1, 3);
+    }
+}
